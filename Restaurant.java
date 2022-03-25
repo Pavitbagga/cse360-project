683e65a5f388cc763c79559e829ae6cc9e087150
roshnideb <99225297+roshnideb@users.noreply.github.com>
2022-03-25 12:45:53 -0700

    Add files via upload

---
+++ Restaurant.java
@@ -0,0 +1,32 @@
+public class Restaurant extends User{
+    String restaurantName;
+    int permissions;
+
+    public Restaurant(String newFirstName, String newLastName, String newUsername, String newPassword, String newUserID, String newRestaurantName, int newPermissions) {
+        this.firstName = newFirstName;
+        this.lastName = newLastName;
+        this.username = newUsername;
+        this.password = newPassword;
+        this.userId = newUserID;
+        this.restaurantName = newRestaurantName;
+        this.permissions = newPermissions;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public boolean setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+        return true;
+    }
+
+    public int getPermissions() {
+        return permissions;
+    }
+
+    public boolean setPermissions(int permissions) {
+        this.permissions = permissions;
+        return true;
+    }
+}
