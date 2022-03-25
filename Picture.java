3ce6c11d42561552e4fc288285309626213c7446
keeleybond <99225833+keeleybond@users.noreply.github.com>
2022-03-25 11:58:08 -0700

    Add files via upload

---
+++ Picture.java
@@ -0,0 +1,28 @@
+package application;
+import java.lang.*;
+import java.util.*;
+
+class Picture { 
+    private String url;
+    private String alternativeText;
+  
+    public Picture(String newUrl,String newAlternativeText){
+      this.url = newUrl;
+      this.alternativeText = newAlternativeText;
+      
+    }
+    public Boolean setUrl (String newUrl){
+      this.url = newUrl;
+      return true;
+    }
+    public Boolean setAlternativeText (String newAlternativeText){
+      this.alternativeText = newAlternativeText;
+      return true;
+    }
+    public String getUrl (){
+      return url;
+    }
+    public String getAlternativeText (){
+      return alternativeText;
+    }
+}
