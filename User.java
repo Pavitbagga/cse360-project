ad74606b16918eba3cc25328b7b75562725cb74d
katykay <99226968+katykay@users.noreply.github.com>
2022-03-25 19:42:23 -0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,66 @@
+// Coded by Katelyn Vanderwolde
+// Last Modified 3/14/22
+// Basic storage class for saving user
+
+public class User {
+	
+	protected String firstName; // protected or private?
+	protected String lastName;
+	protected String userName;
+	protected String password;
+	protected int userId;
+	
+	
+	public User(String newFirstName, String newLastName, String newUserName, String newPassword, int newUserId) {
+		this.firstName = newFirstName;
+		this.lastName = newLastName;
+		this.userName = newUserName;
+		this.password = newPassword;
+		this.userId = newUserId;
+		
+		
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+
+}
