eb4e88c2c999ef83e6b50e848ffa4b925ff6c749
keeleybond <99225833+keeleybond@users.noreply.github.com>
2022-03-28 19:10:15 -0700

    Update MenuView.java

--- MenuView.java
+++ MenuView.java
@@ -1,48 +1,87 @@
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 import java.awt.*;
 import java.util.ArrayList;
 
 public class MenuView extends BaseModel{
-    HBox menuItems;
-    HBox wholeView;
-    HBox searchButtons;
-    HBox searchBar;
-    TextField search;
-    Button searchEnter;
+    HBox menuItems = new HBox();
+    HBox searchButtons = new HBox();
+    HBox searchBar = new HBox();
+    TextField search = new TextField();
+    Button searchEnter = new Button("search");
+    GridPane contentView;
 
     public MenuView(int xSize, int ySize, ArrayList<Button> topButtons, String newPageTitle, Image newLogo) {
 
         super(xSize, ySize, topButtons, newPageTitle, newLogo);
-
-        wholeView = new HBox();
+        contentView = new GridPane();
         BorderPane root = new BorderPane();
-        ScrollPane scroll = new ScrollPane();
+        ScrollBar scroll = new ScrollBar();
 
         //scroll bar
-        menuItems = new HBox();
         root.setCenter(scroll);
-        scroll.setContent(menuItems);
+        scroll.setOrientation(Orientation.VERTICAL);
 
-        //search bar
-        searchBar = new HBox();
-        search = new TextField();
-        searchEnter = new Button("search");
-        searchButtons = new HBox();
+
+        ColumnConstraints col0 = new ColumnConstraints();
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        ColumnConstraints col3 = new ColumnConstraints();
+
+
+        col0.setPercentWidth(15.0);
+        col1.setPercentWidth(60.0);
+        col2.setPercentWidth(10.0);
+        col3.setPercentWidth(15.0);
+
+
+        columns.add(col0);
+        columns.add(col1);
+        columns.add(col2);
+        columns.add(col3);
+
+
+        RowConstraints row0 = new RowConstraints();
+        RowConstraints row1 = new RowConstraints();
+        RowConstraints row2 = new RowConstraints();
+        RowConstraints row3 = new RowConstraints();
+
+
+        row0.setPercentHeight(15.0);
+        row1.setPercentHeight(60.0);
+        row2.setPercentHeight(10.0);
+        row3.setPercentHeight(15.0);
+
+
+        rows.add(row0);
+        rows.add(row1);
+        rows.add(row2);
+        rows.add(row3);
+
+
+        contentView.getRowConstraints().addAll(rows);
+        contentView.getColumnConstraints().addAll(columns);
+
 
         searchBar.getChildren().addAll(search);
         searchButtons.getChildren().addAll(searchBar,searchEnter);
-        wholeView.getChildren().addAll(searchButtons,menuItems,scroll);
 
-        this.mainView.add(wholeView, 0, 1);
+        contentView.add(searchBar,2,1);
+        contentView.add(searchButtons,3,1);
+        contentView.add(scroll,5,1);
+
+        contentView.setHgap(5);
+        contentView.setVgap(5);
+        contentView.setAlignment(Pos.CENTER);
+
+        this.mainView.add(contentView, 0, 1);
     }
 }
